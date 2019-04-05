ded2f5d098c2e467b765f9c6d785183bf10a8b71
sms7182 <sms7182@gmail.com>
2019-04-05 12:34:44 +0430

    fix error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const  SocketServer=require('ws').Server;
-const  SS=new WebSocketServer({port:7575});
+const  SS=new SocketServer({port:7575});
 
 SS.on('connection',(ws)=>{
     ws.on('message',(msg)=>{
