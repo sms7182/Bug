e0786aa0c9de31d66d830455ba34c5b37b78d91a
sms7182 <sms7182@gmail.com>
2019-04-05 18:55:33 +0430

    remove pkg json

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const  cs=new WebSocket('ws://localhost:7575');
- cs.onmessage=(payload)=>{
+let btnClick=false;
+cs.onmessage=(payload)=>{
 
     if(payload){
        var obj= JSON.parse(payload.data);
@@ -7,18 +8,25 @@ const  cs=new WebSocket('ws://localhost:7575');
         if(obj&&obj.objTitle&&obj.objTitle=='Registration'&&obj.data=='1'){
            let h1=document.createElement('h1');
            h1.innerText='connected To server and sign';
-
+           btnClick=true;
+            disableSign();
            document.querySelector('div.messages').appendChild(h1);
        }
     }
  };
+function disableSign() {
+  var btn=  document.getElementById('sign');
+ var  txtinp=document.getElementById('user');
+ txtinp.disabled=true;
+// txtinp.readonly=true;
+ btn.disabled=true;
+ //btn.readonly=true;
+}
 function Click(){
-    var btnClick=false;
+
     console.log('clicked start')
     if(btnClick){
 
-
-        console.log('clicked');
         return;
     }
     else{
