76959ceb964fde518a9b99cb2b746901a1b5272f
sms7182 <sms7182@gmail.com>
2019-04-05 18:11:59 +0430

    connected is ok

--- client.js
+++ client.js
@@ -1,10 +1,13 @@
 const  cs=new WebSocket('ws://localhost:7575');
  cs.onmessage=(payload)=>{
-    if(payload&&payload.objTitle){
 
-       if(payload.objTitle=='Registration'&&payload.data=='1'){
+    if(payload){
+       var obj= JSON.parse(payload.data);
+
+        if(obj&&obj.objTitle&&obj.objTitle=='Registration'&&obj.data=='1'){
            let h1=document.createElement('h1');
-           h1.innerText='Sign';
+           h1.innerText='connected To server and sign';
+
            document.querySelector('div.messages').appendChild(h1);
        }
     }
