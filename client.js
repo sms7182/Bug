5671a00b6da3d7d904c7c07b865d3b85826ff9e6
sms7182 <sms7182@gmail.com>
2019-04-05 18:01:06 +0430

    add mongo db and connection client and server for chat room and logging that

--- client.js
+++ client.js
@@ -1,6 +1,33 @@
 const  cs=new WebSocket('ws://localhost:7575');
+ cs.onmessage=(payload)=>{
+    if(payload&&payload.objTitle){
 
-document.forms[0].onsubmit=()=>{
+       if(payload.objTitle=='Registration'&&payload.data=='1'){
+           let h1=document.createElement('h1');
+           h1.innerText='Sign';
+           document.querySelector('div.messages').appendChild(h1);
+       }
+    }
+ };
+function Click(){
+    var btnClick=false;
+    console.log('clicked start')
+    if(btnClick){
+
+
+        console.log('clicked');
+        return;
+    }
+    else{
+        let usr= document.getElementById('user');
+        var parsd=JSON.stringify({objTitle:'userName', data:usr.value});
+        cs.send(parsd);
+
+
+    }
+}
+
+/*document.forms[0].onsubmit=()=>{
     let  msg=document.getElementById('message');
     cs.send(msg.value);
-}
+}*/
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const  SocketServer=require('ws').Server;
 const  SS=new SocketServer({port:7575});
+var connectToDb=false;
 
+webSockets = {};
 SS.on('connection',(ws)=>{
+    var userID = ws._socket.remotePort;
+    console.log(userID);
+    webSockets[userID] = ws;
     ws.on('message',(msg)=>{
-        console.log(msg);
+    var parsedObj=JSON.parse(msg);
+        if(parsedObj.objTitle=='userName'){
+            console.log(parsedObj.data);
+            registerd(parsedObj.data);
+            ws.send(JSON.stringify({objTitle:'Registration', data:'1'}));
+        }
+
     });
     console.log('Server is Connected');
-})
+});
+function registerd(user) {
+
+}
